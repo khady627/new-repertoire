efc95eee6d4c495105d3160115dff599c5ba8538
Khady <ndourkhady384@gmail.com>
2021-05-01 16:28:04 +0000

    initil commit

---
+++ app/src/main/java/com/example/covid_projet_final/A_propos.java
@@ -0,0 +1,12 @@
+package com.example.covid_projet_final;
+
+import android.os.Bundle;
+
+public class A_propos extends menu {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_propos);
+    }
+}
